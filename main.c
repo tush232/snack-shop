f365dfa92ba7d9df0dfe4b9a35596c1d906157d9
tush232 <105638058+tush232@users.noreply.github.com>
2022-07-04 13:41:20 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void menu();
+int main()
+{
+    printf("\twelcome to zetech c snack shop!\n");
+    menu()
+    return 0;
+
+
+
+    void menu()
+    int item;
+    printf("\tToday's Menu\n");
+    printf("1. Hamburger - Ksh,50\n");
+    printf("2. samosa - Ksh. 50\n");
+    printf("3. coffee - Ksh. 40\n");
+    printf("Enter item No: ");
+    scanf("%d",%item);
+    if(item > 0 && item <= 3)(printf("item added to cart\n);)
+    else
+        printf("invalid item\n");)
+        return item;
